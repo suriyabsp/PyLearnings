cb7cc0433af68cbee7da995b805fa18ffba9d507
suriyabsp <bs.bsuriya@gmail.com>
2023-08-05 18:00:57 +0530

    Hello World with Python
    Trying out hello world strings & print

---
+++ First Demo.py
@@ -0,0 +1,18 @@
+print("hello")
+
+# here are the comments i have defined
+
+a = 7
+print(a)
+
+Str = "Happy Birthday"
+print(Str)
+
+a, b, c = 5, 10.6, "Happy"
+print(a,b,c)
+
+print("{} {}". format("Value is", b))
+
+print(type(b))
+print(type(a))
+print(type(c))
