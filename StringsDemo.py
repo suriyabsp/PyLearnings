970f4710e8892cc92451c76455e9da968a20261e
suriyabsp <bs.bsuriya@gmail.com>
2023-08-07 08:05:50 +0530

    Strings and its basics

---
+++ StringsDemo.py
@@ -0,0 +1,21 @@
+str = "Im learning python"
+
+str1 = "with pycharm"
+
+str2 = "learning"
+
+print(str[1])
+print(str[0:10])   #Substring in python
+print(str+ str1)  #concatenation
+print(str2 in str)  #substring check
+
+
+var = (str.split("r"))
+print(var)
+print(var[0])
+
+
+str3 = " Hi "
+print(str3.strip())
+print(str3.lstrip())
+print(str3.rstrip())
